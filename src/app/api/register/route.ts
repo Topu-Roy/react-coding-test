5b0fa99daa0adb6821aa3074679cc109ffed2863
Topu-Roy <topuroy143333@gmail.com>
2023-09-13 01:51:39 +0600

    seed data to MongoDB Atlas

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,3 +1,32 @@
+import { options } from "@/constants";
+import { prisma } from "../../../../prisma/prismaClient";
+import { NextResponse } from "next/server";
+
 export const POST = async (req: Request) => {
-  console.log("Register route");
+  async function seedData() {
+    for (const { value, label } of options) {
+      await prisma.allSector.create({
+        data: {
+          value,
+          label,
+        },
+      });
+    }
+  }
+  try {
+    await prisma.$connect();
+    seedData();
+    return NextResponse.json(
+      { successMessage: "The data was sent successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { successMessage: "Server error" },
+      { status: 500 }
+    );
+  } finally {
+    prisma.$disconnect();
+  }
 };
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -18,10 +18,25 @@ const Form = () => {
         formState: { errors, isSubmitting, isSubmitted },
     } = useForm<Inputs>();
 
-    const onSubmit: SubmitHandler<Inputs> = (data) => {
-        console.log(data);
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        try {
+            const response = await fetch("/api/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                console.log('data sent successfully')
+            }
+        } catch (error) {
+            console.log('something went wrong')
+        }
+        console.log(data)
         reset();
-    };
+    }
     return (
         <>
             <span className="font-bold text-2xl pb-8">
